5f2db4a6ec92f37951d54aea7456c1661a745e76
ThyagoLuisBorges <thyagoluisborges@gmail.com>
2020-09-09 17:58:18 -0300

    Criado SplashScreen e a activity Heroes

---
+++ app/src/main/java/beca/thyagoluis/becamobile/presentation/heroes/HeroesActivity.kt
@@ -0,0 +1,12 @@
+package beca.thyagoluis.becamobile.presentation.heroes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import beca.thyagoluis.becamobile.R
+
+class HeroesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heroes)
+    }
+}
