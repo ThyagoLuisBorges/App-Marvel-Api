12562681b28786e5557d6479ed9052f69ea4e0f2
ThyagoLuisBorges <thyagoluisborges@gmail.com>
2020-09-10 16:10:51 -0300

    Fixado estrutura de chamada da api

--- app/src/main/java/beca/thyagoluis/becamobile/data/response/HeroesDataResponse.kt
+++ app/src/main/java/beca/thyagoluis/becamobile/data/response/HeroesDataResponse.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class HeroesBodyResponse(
-    @Json(name = "data")
-    val data: HeroesDataResponse
+data class HeroesDataResponse(
+    @Json(name = "results")
+    val results: List<HeroesResultsResponse>
 )
