5c0f3bef4f37e5d1f959dfaf04e4b2f202fa90a7
ThyagoLuisBorges <thyagoluisborges@gmail.com>
2020-09-14 08:53:01 -0300

    Adicionado no adapter funcao de click para abrir tela de detalhes do personagem

--- app/src/main/java/beca/thyagoluis/becamobile/presentation/heroes/HeroesAdapter.kt
+++ app/src/main/java/beca/thyagoluis/becamobile/presentation/heroes/HeroesAdapter.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import beca.thyagoluis.becamobile.R
 import beca.thyagoluis.becamobile.data.model.Hero
@@ -13,7 +14,10 @@ import beca.thyagoluis.becamobile.data.response.HeroesResultsResponse
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.heroes_list.view.*
 
-class HeroesAdapter(private val heroes: List<Hero>) : RecyclerView.Adapter<HeroesAdapter.HeroesViewHolder>() {
+class HeroesAdapter(
+    private val heroes: List<Hero>,
+    private val onItemClickListener: ((hero: Hero) -> Unit)
+) : RecyclerView.Adapter<HeroesAdapter.HeroesViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroesViewHolder {
@@ -21,7 +25,7 @@ class HeroesAdapter(private val heroes: List<Hero>) : RecyclerView.Adapter<Heroe
             .from(parent.context)
             .inflate(R.layout.heroes_list, parent, false)
 
-        return HeroesViewHolder(itemView)
+        return HeroesViewHolder(itemView, onItemClickListener)
     }
 
     override fun onBindViewHolder(holder: HeroesViewHolder, position: Int) {
@@ -30,7 +34,8 @@ class HeroesAdapter(private val heroes: List<Hero>) : RecyclerView.Adapter<Heroe
 
     override fun getItemCount() = heroes.count()
 
-    class HeroesViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+    class HeroesViewHolder(view: View, private val onItemClickListener: ((hero: Hero) -> Unit)) :
+        RecyclerView.ViewHolder(view) {
 
         private val heroName = view.textHeroName
         private val imageAvatar = view.imageViewHero
@@ -38,13 +43,17 @@ class HeroesAdapter(private val heroes: List<Hero>) : RecyclerView.Adapter<Heroe
         fun bindView(hero: Hero) {
             heroName.text = hero.name
 
-            val image = hero.thumbnail.split(":")
+            val thumbnail = hero.thumbnail.split(":")
 
             Glide
                 .with(itemView)
-                .load("https:" + image[1])
+                .load("https:" + thumbnail[1])
                 .centerCrop()
                 .into(imageAvatar);
+
+            itemView.setOnClickListener {
+                onItemClickListener.invoke(hero)
+            }
         }
     }
 }
